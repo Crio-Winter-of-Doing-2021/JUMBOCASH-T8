0ea17e85ca36a3ad8af3d3364838eac7703c0dd4
sukanta-nandi <sukantanandi0@gmail.com>
2021-03-12 02:41:59 +0530

    Revert "created retailer serializer and views"
    This reverts commit 40285338401ad8f47c798d9ca1cabc8899f4778a.

--- jumbocashapi/views.py
+++ jumbocashapi/views.py
@@ -1,18 +1,3 @@
 from django.shortcuts import render
-from .models import Retailer
-from .serializers import RetailerSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-
- 
-class RetailerListCreateView(ListCreateAPIView):
-    """Handles List and Create of a Retailer object"""
-    queryset            = Retailer.objects.all()
-    serializer_class    = RetailerSerializer
-
-
-
-class RetailerUpdateRetriveDeleteView(RetrieveUpdateDestroyAPIView):
-    """Handles update, retrive and delete of retailer obj"""
-    queryset            = Retailer.objects.all()
-    serializer_class    = RetailerSerializer
+# Create your views here.
