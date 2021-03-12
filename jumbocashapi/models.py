d647651a341fa66729f89284971879caf9b62620
sukanta-nandi <sukantanandi0@gmail.com>
2021-03-13 01:15:21 +0530

    created required models

--- jumbocashapi/models.py
+++ jumbocashapi/models.py
@@ -4,6 +4,30 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
+payment_mode_choices = (
+    ('1', 'Cash'),
+    ('2', 'Card'),
+    ('3', 'UPI'),
+    ('4', 'Other Online Mode')
+    
+)
+
+inc_payment_status_choices = (
+    ('1', 'Pending'),
+    ('2', 'Received')
+)
+
+exp_payment_status_choices = (
+    ('1', 'Pending'),
+    ('2', 'Paid')
+)
+
+trans_type_choices = (
+    ('1', 'income'),
+    ('2', 'expense')
+)
+
+
 class RetailerManager(BaseUserManager):
     """Manager for Retailer"""
 
@@ -59,4 +83,61 @@ class Retailer(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+class Customer(models.Model):
+    """Model for customer enity (cash inflow entity)"""
+    firstname   = models.CharField(max_length=255)
+    lastname    = models.CharField(max_length=255, blank=True)
+    mobile_no   = models.CharField(max_length=10, blank=True)
+    email_id    = models.EmailField(max_length=255, unique=True, blank=True)
+    ret_id      = models.ForeignKey(Retailer, on_delete=models.CASCADE)
 
+    REQUIRED_FIELDS = ['firstname']
+
+    def __str__(self):
+        """Return string representation of customer"""
+        return self.firstname
+
+class Supplier(models.Model):
+    """Model for customer enity (cash outflow entity)"""
+    firstname   = models.CharField(max_length=255)
+    lastname    = models.CharField(max_length=255, blank=True)
+    mobile_no   = models.CharField(max_length=10, blank=True)
+    email_id    = models.EmailField(max_length=255, unique=True, blank=True)
+    ret_id      = models.ForeignKey(Retailer, on_delete=models.CASCADE)
+
+    REQUIRED_FIELDS = ['firstname']
+
+    def __str__(self):
+        """Return string representation of supplier"""
+        return self.firstname
+
+class IncomeTransaction(models.Model):
+    """Model for cash inflow transaction entity"""
+    trans_date_time  = models.DateTimeField(auto_now_add=True, blank=True)
+    amount           = models.IntegerField()
+    note             = models.CharField(max_length=255)
+    description      = models.CharField(max_length=255, blank=True)
+    payment_mode     = models.CharField(max_length=50, choices=payment_mode_choices)
+    payment_status   = models.CharField(max_length=50, choices=inc_payment_status_choices)
+    due_date         = models.DateField(blank=True, null=True)
+    cust_id          = models.ForeignKey(Customer, on_delete=models.CASCADE)
+ 
+
+    def __str__(self):
+        """Return string representation of incomeTransaction"""
+        return self.note + "    "+ str(self.trans_date_time)
+
+class ExpenseTransaction(models.Model):
+    """Model for cash outflow transaction entity"""
+    trans_date_time  = models.DateTimeField(auto_now_add=True, blank=True)
+    amount           = models.IntegerField()
+    note             = models.CharField(max_length=255)
+    description      = models.CharField(max_length=255, blank=True)
+    payment_mode     = models.CharField(max_length=50, choices=payment_mode_choices)
+    payment_status   = models.CharField(max_length=50, choices=exp_payment_status_choices)
+    due_date         = models.DateField(blank=True, null=True)
+    sup_id           = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return string representation of expenseTransaction"""
+        return self.note + "    "+ str(self.trans_date_time)
