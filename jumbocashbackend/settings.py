242f75e896b083c4ecfc4f6b2914170443a10a1d
sukanta-nandi <sukantanandi0@gmail.com>
2021-03-24 21:56:54 +0530

    authentication added

--- jumbocashbackend/settings.py
+++ jumbocashbackend/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
