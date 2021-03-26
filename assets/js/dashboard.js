2a1c2275aa48cf27864ace22dd1e215200922d76
Rudrakshi soni <sonirudrakshi99@gmail.com>
2021-03-26 23:04:43 +0530

    removed token

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,34 +1,34 @@
 let tk = localStorage.getItem("token");
+localStorage.removeItem("token");
+// if (tk) {
+//     const myHeaders = new Headers();
 
-if (tk) {
-    const myHeaders = new Headers();
+//     myHeaders.append('Content-Type', 'application/json');
+//     myHeaders.append('Authorization', 'tk');
+//     fetch("http://127.0.0.1:8000/jumbocashapi/", {
+//             method: 'GET',
+//             headers: myHeaders,
+//         })
+//         .then((response) => {
+//             /* Error Handling */
 
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Authorization', 'tk');
-    fetch("http://127.0.0.1:8000/jumbocashapi/", {
-            method: 'GET',
-            headers: myHeaders,
-        })
-        .then((response) => {
-            /* Error Handling */
+//             if (response.status === 401) {
+//                 window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
+//                 throw Error(response.status);
 
-            if (response.status === 401) {
-                window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
-                throw Error(response.status);
+//             }
+//             console.log(response.status);
+//             return response.json();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
 
-            }
-            console.log(response.status);
-            return response.json();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-}
-const logout = document.getElementById("logout");
-logout.addEventListener("click", (e) => {
-    localStorage.removeItem("token");
-})
+// }
+// const logout = document.getElementById("logout");
+// logout.addEventListener("click", (e) => {
+//     localStorage.removeItem("token");
+// })
 
 
 // if (token) {
