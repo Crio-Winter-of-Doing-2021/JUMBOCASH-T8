83b7a5a090fe1d1b21f82bff34747a5b24fb1c19
Rudrakshi <sonirudrakshi99@gmail.com>
2021-03-26 23:08:39 +0530

    removed token (#57)
    * added localstorage
    
    * fixed bug
    
    * added dashboard.js
    
    * update
    
    * added logout
    
    * added page relocation
    
    * removed token

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,37 +1,35 @@
 let tk = localStorage.getItem("token");
 
-if (tk) {
-    const myHeaders = new Headers();
+localStorage.removeItem("token");
+// if (tk) {
+//     const myHeaders = new Headers();
 
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Authorization', 'tk');
-    fetch("http://127.0.0.1:8000/jumbocashapi/", {
-            method: 'GET',
-            headers: myHeaders,
-        })
-        .then((response) => {
-            /* Error Handling */
+//     myHeaders.append('Content-Type', 'application/json');
+//     myHeaders.append('Authorization', 'tk');
+//     fetch("http://127.0.0.1:8000/jumbocashapi/", {
+//             method: 'GET',
+//             headers: myHeaders,
+//         })
+//         .then((response) => {
+//             /* Error Handling */
 
-            if (response.status === 401) {
+//             if (response.status === 401) {
+//                 window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
+//                 throw Error(response.status);
 
-                window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
-                throw Error(response.status);
+//             }
+//             console.log(response.status);
+//             return response.json();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
 
-            }
-            console.log(response.status);
-            return response.json();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-}
-
-const logout = document.getElementById("logout");
-logout.addEventListener("click", (e) => {
-    localStorage.removeItem("token");
-})
-
+// }
+// const logout = document.getElementById("logout");
+// logout.addEventListener("click", (e) => {
+//     localStorage.removeItem("token");
+// })
 
 // if (token) {
 
