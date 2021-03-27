9fb2995b2f8698d1e9f10a2f34958dde3a708eac
Rudrakshi soni <sonirudrakshi99@gmail.com>
2021-03-27 16:07:22 +0530

    removed bug

--- assets/js/dashboard-preload.js
+++ assets/js/dashboard-preload.js
@@ -1,42 +1,25 @@
-let tk = localStorage.getItem("token");
+var tk = localStorage.getItem("token");
 
-if (tk) {
-    const myHeaders = new Headers();
+console.log(JSON.parse(tk));
+var p = JSON.parse(tk);
+console.log(p);
+console.log(typeof(p));
 
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Authorization', 'Token ' + tk);
+fetch("http://127.0.0.1:8000/jumbocashapi/", {
+        method: 'GET',
+        headers: { "Authorization": "Token " + p },
+    })
+    .then((response) => {
+        /* Error Handling */
 
-    fetch("http://127.0.0.1:8000/jumbocashapi/", {
-            method: 'GET',
-            headers: myHeaders,
-        })
-        .then((response) => {
-            /* Error Handling */
+        if (response.status === 401) {
+            window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
+            throw Error(response.status);
 
-            if (response.status === 401) {
-                window.location.href = "https://jumbocashflow-app-t8.netlify.app/";
-                throw Error(response.status);
-
-            }
-            console.log(response.status);
-            return response.json();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-}
-// const logout = document.getElementById("logout");
-// logout.addEventListener("click", (e) => {
-//     localStorage.removeItem("token");
-// })
-
-// // if (token) {
-
-// //     res = // call /jumbocashi api endpoint with token
-
-// //         if res = true
-// //     redirect to dashboard
-// //     else
-// //         redirect to login
-// // }
+        }
+        console.log(response.status);
+        return response.json();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
