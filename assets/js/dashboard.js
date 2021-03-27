f3ad8509b6d3a396499e76a25a25aa15fcf52f17
Rudrakshi soni <sonirudrakshi99@gmail.com>
2021-03-27 23:33:21 +0530

    added features in static dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,3 +2,15 @@ const logout = document.getElementById("logout");
 logout.addEventListener("click", (e) => {
     localStorage.removeItem("token");
 })
+const backToTopButton = document.querySelector("#back-to-top-btn");
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 200) { //make the button visible
+        backToTopButton.style.display = "block";
+    } else { // hide the button
+        backToTopButton.style.display = "none";
+    }
+});
+
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
