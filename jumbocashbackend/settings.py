989be6ee7139d319d5201f64f8e474aee909d87c
sukanta-nandi <sukantanandi0@gmail.com>
2021-03-28 12:40:43 +0530

    added static root

--- jumbocashbackend/settings.py
+++ jumbocashbackend/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'multiple_permissions.middlewares.PermissionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jumbocashbackend.urls'
@@ -166,6 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'jumbocashapi.Retailer'
