dd6546452238e159c2da5d0b727e98c269164079
rudrakshi9 <rudrakshi.soni@createbytes.com>
2021-11-30 12:53:16 +0530

    remove the debugging statements

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,6 @@
 const logout = document.querySelector(".logout");
 logout.addEventListener("click", (e) => {
-    console.log(logout);
+    // console.log(logout);
     localStorage.removeItem("token");
 })
 
@@ -41,7 +41,6 @@ const display_profile = (data) => {
 }
 var tk = localStorage.getItem("token");
 
-console.log(JSON.parse(tk));
 var p = JSON.parse(tk);
 
 fetch("https://jumbocashapi.herokuapp.com/retailers/profile/", {
